c19fd773d2b91292ff0844fb28d0e535f1a886a6
James Cocks <j.cocks543@gmail.com.au>
2020-02-05 22:50:38 +1100

    Add database tables for pets and owners, and a lookup table for animal types

---
+++ PetRegoDB/tblOwners.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tblOwners]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(50) NULL, 
+    [LastName] NCHAR(50) NULL
+)
---
+++ PetRegoDB/tblPets.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblPets]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FKOwnerId] UNIQUEIDENTIFIER NULL, 
+    [FKAnimalTypeId] INT NULL, 
+    [Name] NCHAR(50) NULL, 
+    CONSTRAINT [FK_tblPet_tblOwners] FOREIGN KEY ([FKOwnerId]) REFERENCES tblOwners(Id), 
+    CONSTRAINT [FKAnimalTypeId] FOREIGN KEY ([FKAnimalTypeId]) REFERENCES [LkpAnimal]([Id])
+)

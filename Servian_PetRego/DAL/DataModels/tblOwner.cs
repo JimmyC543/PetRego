1f73da993f8176788096a73bb2645736b3d7eaf3
James Cocks <j.cocks543@gmail.com.au>
2020-02-05 23:14:12 +1100

    Add data object models for DB tables

---
+++ Servian_PetRego/DAL/DataModels/tblOwner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Servian_PetRego.DAL.DataModels
+{
+    public class tblOwner
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<tblPet> Pets { get; private set; }
+    }
+}

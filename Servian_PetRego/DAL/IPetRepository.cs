e4a2250a589ab53f5e33d79993db62b0ded1063c
James Cocks <j.cocks543@gmail.com.au>
2020-02-05 23:50:24 +1100

    Add general and specific repository interfaces for later on

---
+++ Servian_PetRego/DAL/IPetRepository.cs
@@ -0,0 +1,13 @@
+﻿using Servian_PetRego.DAL.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Servian_PetRego.DAL
+{
+    public interface IPetRepository : IRepository<tblPet>
+    {
+        public tblOwner GetOwnerByPetId(Guid petId);
+    }
+}
---
+++ Servian_PetRego/DAL/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Servian_PetRego.DAL
+{
+    public interface IRepository<T> where T : class
+    {
+        public T GetById(Guid id);
+        public IEnumerable<T> GetAll();
+        public void Add(T entity);
+        public void AddRange(IEnumerable<T> entities);
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        public void Remove(T entity);
+        public void RemoveRange(IEnumerable<T> entities);
+    }
+}

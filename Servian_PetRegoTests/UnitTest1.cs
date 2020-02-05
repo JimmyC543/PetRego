c151646faa083a94940f0082f8c135c02d3020b7
James Cocks <j.cocks543@gmail.com.au>
2020-02-06 00:12:42 +1100

    Add NUnit project

---
+++ Servian_PetRegoTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Servian_PetRegoTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

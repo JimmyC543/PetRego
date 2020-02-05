399ce045711d73eeee69746da3eff9fcc6a15b98
James Cocks <j.cocks543@gmail.com.au>
2020-02-05 23:45:16 +1100

    Add db context, and configure it in the Startup for dependency injection

---
+++ Servian_PetRego/DAL/PetRegoDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Servian_PetRego.DAL.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Servian_PetRego.DAL
+{
+    public class PetRegoDbContext : DbContext
+    {
+        //TODO: try feeding in an IDbContextOptions argument, if possible
+        public PetRegoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<tblOwner> Owners { get; set; }
+        public DbSet<tblPet> Pets { get; set; }
+        public DbSet<LkpAnimalType> AnimalTypes { get; set; }
+    }
+}
--- Servian_PetRego/Startup.cs
+++ Servian_PetRego/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Servian_PetRego.DAL;
+using Microsoft.EntityFrameworkCore;
 
 namespace Servian_PetRego
 {
@@ -26,6 +28,9 @@ namespace Servian_PetRego
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //TODO: Add default command timeout?
+            services.AddDbContext<PetRegoDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

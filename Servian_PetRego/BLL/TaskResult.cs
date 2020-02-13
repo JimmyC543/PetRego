8b13c8f11d60871c08c675510cec916c37826b9c
James Cocks <j.cocks543@gmail.com.au>
2020-02-13 18:59:29 +1100

    Add TaskResult class for better communication between the business layer and the presentation layer

---
+++ Servian_PetRego/BLL/TaskResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetRego.BLL
+{
+    public class TaskResult
+    {
+        public object Data { get; set; }
+
+        public bool Success { get; set; } = true;
+
+        //This could be expanded, perhaps to contain exceptions, or a ValidationMessage class
+        public IEnumerable<string> ValidationMessages { get; set; }
+
+    }
+}
